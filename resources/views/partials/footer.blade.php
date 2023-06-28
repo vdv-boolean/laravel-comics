acac1d736815c559abd9d4431efe6f7e8f0e766d
vincenzodivita <divitavincenzo@icloud.com>
2023-06-28 17:57:44 +0200

    Add footer code

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,88 @@
+<footer>
+    <div class="container-infos">
+        <div class="infos">
+            <div class="info">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="info">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="info">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="info">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="info">
+                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-specs">
+        <div class="container">
+            <div class="specs">
+                <ul>
+                <li class="subtitle">DC COMICS</li>
+                <li>Charaters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+                <li class="subtitle">SHOP</li>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+                </ul>
+
+                <ul>
+                <li class="subtitle">DC</li>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+                </ul>
+
+                <ul>
+                <li class="subtitle">SITES</li>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+                </ul>
+            </div>
+
+            <div class="dc-logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+      </div> 
+    </div>
+
+    <div class="socials">
+        <div class="container">
+            <button>SIGN UP NOW!</button>
+            <div class="platforms">
+                <span>FOLLOW US</span>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
