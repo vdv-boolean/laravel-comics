e4cc2ab68d23065fb5168ff36fa1019855eef50e
vincenzodivita <divitavincenzo@icloud.com>
2023-06-28 17:35:57 +0200

    Add header partial

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+    <nav>
+        <ul class="nav-list">
+            <li class="nav-item">CHARACTERS</li>
+            <li class="nav-item">COMICS</li>
+            <li class="nav-item">MOVIES</li>
+            <li class="nav-item">TV</li>
+            <li class="nav-item">GAMES</li>
+            <li class="nav-item">COLLECTIBLES</li>
+            <li class="nav-item">VIDEOS</li>
+            <li class="nav-item">FANS</li>
+            <li class="nav-item">NEWS</li>
+            <li class="nav-item">SHOP</li>
+            <li class="nav-item"><input class="form-control" type="text" placeholder="Search"></li>
+        </ul>
+    </nav>   
+</header>
